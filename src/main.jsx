4eafc7e61af731eb4e56f200a9be665781d3a7e6
Mamadou Elimane Wane <mamadouelimane.dia@gmail.com>
2026-08-27 19:18:42 +0100

    Trigger Vercel sync with v1.0.1 production deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// v1.0.1 - Production deployment with Leaflet map
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
